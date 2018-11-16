b0d722bd08e4213404688800cdbbc632a6e5dec6
denandserg <denandserg@gmail.com>
2018-11-17 01:15:38 +0200

    Power PC Manager
    Signed-off-by: denandserg <denandserg@gmail.com>

---
+++ PC_Power/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace PC_Power
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.exit_system = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ShutDown";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ShutDown);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(128, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Restart";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Restart);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(128, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 34);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Lock";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Lock);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 64);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 34);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Sleep";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Son);
+            // 
+            // exit_system
+            // 
+            this.exit_system.Location = new System.Drawing.Point(128, 116);
+            this.exit_system.Name = "exit_system";
+            this.exit_system.Size = new System.Drawing.Size(110, 34);
+            this.exit_system.TabIndex = 5;
+            this.exit_system.Text = "Exit Windows";
+            this.exit_system.UseVisualStyleBackColor = true;
+            this.exit_system.Click += new System.EventHandler(this.exit_System);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 116);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(110, 34);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Hyberlife";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.hyberlife);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(253, 169);
+            this.Controls.Add(this.exit_system);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PC_Power";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button exit_system;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ PC_Power/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PC_Power
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ShutDown(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("cmd", "/c shutdown -s -f -t 00");
+        }
+
+        private void Restart(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("cmd", "/c shutdown -r -f -t 00");
+        }
+
+        private void exit_System(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("cmd", "/c shutdown -l -f");
+        }
+
+        private void hyberlife(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("cmd", "/c shutdown -hybrid -s");
+        }
+
+        private void Son(object sender, EventArgs e)
+        {
+            //System.Diagnostics.Process.Start("cmd", "/c shutdown -h -f");
+            Application.SetSuspendState(PowerState.Suspend, true, true);
+        }
+
+        private void Lock(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("cmd", "/c rundll32.exe user32.dll, LockWorkStation"); 
+        }
+    }
+}
